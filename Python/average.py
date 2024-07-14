be1d905570a3284a496b196b35c9b22123d561ef
최수양 <chltndid724@gmail.com>
2024-07-14 21:16:39 +0900

    practice

---
+++ Python/average.py
@@ -0,0 +1,6 @@
+def average(data):
+    result = sum(data) / len(data)
+    return result
+
+
+test = 10
--- Python/python.py
+++ Python/python.py
@@ -29,10 +29,50 @@ box = ['cream1','cream2','cream3']
 # if number > 100 : 
 #     print('over')
 
-num1 = 10
-num2 = 20
+# num1 = 10
+# num2 = 20
 
-if (num1 > num2) :
-    print("Max1", num1)
-else :
-    print("Max2", num2)
+# if (num1 > num2) :
+#     print("Max1", num1)
+# else :
+#     print("Max2", num2)
+
+# cart = ["one","two","three"]
+
+# for i in cart :
+#     print(i)
+
+
+# dic = {
+#     "one" : 1,
+#     "two" : 2,
+#     "three" : 3
+# }
+
+# for k,v in dic.items() :
+#     print(k,v)
+
+# def start():
+#     print("*" * 30)
+#     print("start")
+#     print("*" * 30)
+
+# start()
+
+
+# import average
+
+# average.average.test
+
+# print(average.average([10,20,30]))
+
+
+class bread :
+    def make(kind, put_taste):
+        kind.taste = put_taste
+
+
+my_bread = bread()
+bread.make(my_bread, "spicy")
+
+print(my_bread.taste)
